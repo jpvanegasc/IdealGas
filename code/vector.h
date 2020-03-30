d337f4783d902f9c66e68442ea01406b4a35f3cc
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-03-30 13:04:31 -0500

    Fix: Memory leakage on vector Refactor: No for loops

--- code/vector.h
+++ code/vector.h
@@ -1,15 +1,25 @@
 /**
- * 3D Vector helper class
+ * 3D Vector helper class. 
+ * Vectors are allocated with new, but because operator overloading uses a return statement, 
+ * freeing memory can't be done using the class destructor. Because of this, you 
+ * **must use the free_memory function**
+ * failing to do so will result on memory leakage, because it's the only way this class 
+ * implements it. If you have a solution for this please refer to
+ * www.github.com/jpvanegasc/UsefulPrograms , this project main repository, and check if 
+ * it has been solved already, or make a pull request.
+ * 
  * Author : Jose Daniel Munoz et al.
+ * Modified: Juan Pablo Vanegas
  */
 #include <iostream>
 #include <cmath>
 
 class Vector3D{
     double *v = NULL;
+    //double v[3];
     public:
         Vector3D(double =0.0, double =0.0, double =0.0);
-        ~Vector3D();
+        void free_memory(void);
         void load(double x0, double y0, double z0);
         void show(void);
         /* @return x component*/
@@ -40,7 +50,7 @@ Vector3D::Vector3D(double x, double y, double z){
     v = new double[3];
     load(x,y,z);
 }
-Vector3D::~Vector3D(){
+void Vector3D::free_memory(void){
     delete[] v;
 }
 /* Load vector values */
@@ -51,26 +61,41 @@ void Vector3D::show(void){
     std::cout << "(" << v[0]<< "," << v[1]<< "," << v[2]<< ")" << std::endl;
 }
 Vector3D Vector3D::operator=(Vector3D v2){
-    for(int i=0;i<3;i++)
-        v[i] = v2.v[i];
+    v[0] = v2.v[0];
+    v[1] = v2.v[1];
+    v[2] = v2.v[2];
+    
     return *this;
 }
 Vector3D Vector3D::operator+(Vector3D v2){
-    Vector3D result;
-    for(int i=0;i<3;i++)
-        result.v[i] = v[i] + v2.v[i];
-    return result;
+    v[0] += v2.v[0];
+    v[1] += v2.v[1];
+    v[2] += v2.v[2];
+    
+    return *this;
 }
 Vector3D Vector3D::operator+=(Vector3D v2){
     *this = *this + v2;
     return *this;
 }
+Vector3D Vector3D::operator-(Vector3D v2){
+    v[0] -= v2.v[0];
+    v[1] -= v2.v[1];
+    v[2] -= v2.v[2];
+    
+    return *this;
+}
+Vector3D Vector3D::operator-=(Vector3D v2){
+    *this = *this - v2;
+    return *this;
+}
 /* Vector times scalar */
 Vector3D Vector3D::operator*(double a){
-    Vector3D result;
-    for(int i=0;i<3;i++)
-            result.v[i] = a*v[i];
-    return result;
+    v[0] *= a;
+    v[1] *= a;
+    v[2] *= a;
+    
+    return *this;
 }
 /* Vector times scalar */
 Vector3D Vector3D::operator*=(double a){
@@ -79,25 +104,16 @@ Vector3D Vector3D::operator*=(double a){
 }
 /* Vector divided by scalar */
 Vector3D Vector3D::operator/(double a){
-    double inver = 1.0/a;
-    Vector3D result;
-    for(int i=0;i<3;i++)
-            result.v[i] = inver*v[i];
-    return result;
-}
-Vector3D Vector3D::operator-(Vector3D v2){
-    return *this + v2*(-1); 
-}
-Vector3D Vector3D::operator-=(Vector3D v2){
-    *this = *this - v2;
+    double inverse = 1.0/a;
+    v[0] *= inverse;
+    v[1] *= inverse;
+    v[2] *= inverse;
+    
     return *this;
 }
 /* Dot product */
 double Vector3D::operator*(Vector3D v2){
-    double dot = 0;
-    for(int i=0;i<3;i++)
-            dot += v[i]*v2.v[i];
-    return dot;
+    return v[0]*v2.v[0] + v[1]*v2.v[1] +v [2]*v2.v[2];;
 }
 /* Cross product */
 Vector3D Vector3D::operator^(Vector3D v2){
@@ -105,19 +121,19 @@ Vector3D Vector3D::operator^(Vector3D v2){
     result.v[0] = v[1]*v2.v[2]-v[2]*v2.v[1];
     result.v[1] = v[2]*v2.v[0]-v[0]*v2.v[2];
     result.v[2] = v[0]*v2.v[1]-v[1]*v2.v[0];
-    return result;
+    *this = result;
+    result.free_memory;
+    return *this;
 }
+/* Dont use this. Memory leakage */
 Vector3D operator*(double a, Vector3D v1){
     Vector3D result;
-    result = v1*a;	
+    result = v1*a;
     return result;
 }
 /* @return vector norm squared */
 double norm2(Vector3D v1){
-    double norm = 0;
-    for(int i=0;i<3;i++)
-            norm += v1.v[i]*v1.v[i];
-    return norm;
+    return v1.v[0]*v1.v[0] + v1.v[1]*v1.v[1] + v1.v[2]*v1.v[2];
 }
 /* @return vector norm */
 double norm(Vector3D v1){
