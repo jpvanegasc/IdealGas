9cdcd6843f796ddd71572ab4e3ba27e271846d48
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-04-01 08:56:28 -0500

    Implementation update

--- code/constants.h
+++ code/constants.h
@@ -3,7 +3,7 @@
 #include<fstream>
 
 // Geometry costants
-const double square = 10.0;
+const double square = 50.0;
 const double Lx = square, Ly = square;
 
 // PEFRL
@@ -15,4 +15,8 @@ const double coef1 = (1 - 2*Lambda)/2;
 const double coef2 = (1 - 2*(Xi+Zi));
 
 // Implementation
-const int N = 1;
+const int Nx = 5, Ny = 5;
+const int N = Nx*Ny;
+const double R0 = 1.0, m0 = 1.0;
+const double dt = 1.0e-3;
+const double dx = Lx/(Nx+1), dy = Ly/(Ny+1);
