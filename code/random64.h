8d41616437bed46c95f0a7ce3b5805946bb03ef5
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-07-11 13:08:51 -0500

    Refactor: Optimizations

--- code/random64.h
+++ code/random64.h
@@ -8,12 +8,14 @@ class CRandom{
     unsigned long long u, v, w;
     public:
         CRandom(unsigned long long seed);
-        unsigned long long int64();
-        double r() {return 5.42101086242752217E-20 * int64();}
+        unsigned long long int64(void);
+        double r(void);
         unsigned int int32(){return (unsigned int) int64();};
         double exponencial(float tau);
         double gauss(float mu, float sigma);
 };
+
+inline double CRandom::r(void){return 5.42101086242752217E-20 * int64();}
 
 CRandom::CRandom(unsigned long long seed){
     v = 4101842887655102017LL; w=1;
@@ -21,7 +23,7 @@ CRandom::CRandom(unsigned long long seed){
     v = u; int64();
     w = v; int64();
 }
-unsigned long long CRandom::int64() {
+unsigned long long CRandom::int64(void){
     u = u * 2862933555777941757LL + 7046029254386353087LL;
     v ^= v >> 17; v ^= v << 31; v ^= v >> 8;
     w = 4294957665U*(w & 0xffffffff) + (w >> 32);
@@ -29,10 +31,10 @@ unsigned long long CRandom::int64() {
     
     return (x + v) ^ w;
 }
-double CRandom::exponencial(float tau){
+inline double CRandom::exponencial(float tau){
     return -tau*std::log(r());
 }
-double CRandom:: gauss(float mu, float sigma){
+inline double CRandom:: gauss(float mu, float sigma){
     return sigma*std::sqrt(-2*std::log(r()))*std::cos(2*M_PI*r()) + mu;
 }
 
